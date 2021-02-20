c0486fc270853cc24039cc7273d80b7bf927037d
Luci437 <44019738+Luci437@users.noreply.github.com>
2021-02-20 17:50:49 +0530

    fees update2

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -6,7 +6,4 @@
     $password = '';
 
     $conn = mysqli_connect($serverName,$username,$password,$databaseName);
-    if(mysqli_connect_error($conn)) {
-        die('can\'t connect to database ');
-    }
 ?>
